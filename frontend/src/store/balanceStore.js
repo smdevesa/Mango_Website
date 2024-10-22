8933d1d8a909e242e51d7f1bfba9325c0f3d4c94
TomasBalbo <tbalboakoolen@itba.edu.ar>
2024-10-22 11:56:27 -0300

    Correcto Funcionamiento del Balance Store
    .

--- frontend/src/store/balanceStore.js
+++ frontend/src/store/balanceStore.js
@@ -2,17 +2,26 @@ import { defineStore } from 'pinia'
 
 export const useBalanceStore = defineStore('balance', {
   state: () => ({
-    balance: '$1,234.56',
-    sections: [
-      { label: 'Otros', percentage: 100, color: 'grey' }
-    ]
+    balance: '$0.00',
+    sections: [{label: 'Disponible', percentage: 100, color: 'green'}]
   }),
   actions: {
     updateBalance(newBalance) {
       this.balance = newBalance
     },
-    updateSections(newSections) {
-      this.sections = newSections
+    addSection(newSection) {
+      this.sections.push(newSection)
+      this.recalculatePercentages()
+    },
+    removeSection(index) {
+      this.sections.splice(index, 1)
+      this.recalculatePercentages()
+    },
+    recalculatePercentages() {
+      const total = this.sections.reduce((sum, section) => sum + section.value, 0)
+      this.sections.forEach(section => {
+        section.percentage = (section.value / total) * 100
+      })
     }
   }
 })
